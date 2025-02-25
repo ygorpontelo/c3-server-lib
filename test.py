5f2f7b9af168b3a57646f18758868d3480624e28
ygor <ygorpontelo@gmail.com>
2025-02-25 15:28:19 -0300

    split fix, keep-alive fix, test script update

--- test.py
+++ test.py
@@ -8,36 +8,30 @@ async def make_req(client):
     try:
         r = await client.post("/ping", data=b"Ping!")
         return r.text
-    except Exception:
-        return "error read"
-
+    except Exception as e:
+        return e
 
 async def main(qtd = 100):
     reqs = []
     async with httpx.AsyncClient(base_url="http://localhost:8000", timeout=15) as client:
         for _ in range(qtd):
             reqs.append(make_req(client))
-        # t1 = time.time()
         res = await asyncio.gather(*reqs)
-        # t2 = time.time()
     return res
-    # print(res)
-    # print(res[0])
-    # print(len(res))
-    # print(t2-t1)
-    # print(f"{qtd/(t2-t1)} req/s")
 
 def run_main(qtd):
     loop = asyncio.new_event_loop()
     res = loop.run_until_complete(main(qtd))
-    return res[0]
+    return res
 
 if __name__ == "__main__":
-    p, qtd = 250, 200
+    p, qtd = 200, 150
     v = [qtd for _ in range(p)]
     with ProcessPoolExecutor(max_workers=16) as executor:
         t1 = time.time()
-        r = executor.map(run_main, v)
+        res = executor.map(run_main, v)
     t2 = time.time()
-    print(list(r))
+    for r in res:
+        re = len([v for v in r if isinstance(v, httpx.ReadError)])
+        print((r[0], re))
     print(f"{(qtd*p) / (t2-t1)} req/s")
