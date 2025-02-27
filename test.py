088cf4cf4087f7edb2eea69be0af7c0b3f890cb0
ygor <ygorpontelo@gmail.com>
2025-02-27 01:11:07 -0300

    threads have better result

--- test.py
+++ test.py
@@ -1,38 +1,24 @@
 import time
-import asyncio
 import httpx
-from concurrent.futures import ProcessPoolExecutor
-
-
-async def make_req(client):
-    try:
-        r = await client.post("/ping", data=b"Ping!")
-        return r.text
-    except Exception as e:
-        return e
-
-async def main(qtd = 100):
-    reqs = []
-    async with httpx.AsyncClient(base_url="http://localhost:8000", timeout=15) as client:
-        for _ in range(qtd):
-            reqs.append(make_req(client))
-        res = await asyncio.gather(*reqs)
-    return res
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 def run_main(qtd):
-    loop = asyncio.new_event_loop()
-    res = loop.run_until_complete(main(qtd))
-    return res
+    with httpx.Client(base_url="http://localhost:8000", timeout=15) as client:
+        def req(data):
+            return client.post("/ping", data=data).text
+        with ThreadPoolExecutor(8) as executor:
+            res = executor.map(req, ["Ping!" for _ in range(qtd)])
+    return list(res)
 
 if __name__ == "__main__":
-    p, qtd = 256, 100
+    p, qtd = 512, 512
     v = [qtd for _ in range(p)]
-    # 8 workers seems to work better
-    with ProcessPoolExecutor(max_workers=8) as executor:
+    with ProcessPoolExecutor(max_workers=12) as executor:
         t1 = time.time()
         res = executor.map(run_main, v)
     t2 = time.time()
     for r in res:
         re = len([v for v in r if isinstance(v, httpx.ReadError)])
-        print((r[0], re))
+        if re > 0:
+            print(f"error read: {re}")
     print(f"{(qtd*p) / (t2-t1)} req/s")
